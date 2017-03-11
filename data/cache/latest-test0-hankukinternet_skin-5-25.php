ed72a5022cb1d65285211cb76ff8de5db785081e
HANKUKINTERNET <HANKUKINTERNET@DESKTOP-8GD94SO>
2017-03-12 01:03:07 +0900

    test

---
+++ data/cache/latest-test0-hankukinternet_skin-5-25.php
@@ -0,0 +1,6 @@
+<?php
+if (!defined('_GNUBOARD_')) exit;
+$bo_subject='애니메이션';
+$bo_1='애니메이션 | 매드무비';
+$list=array (
+)?>
---
+++ data/cache/latest-test2-hankukinternet_skin-5-25.php
@@ -0,0 +1,6 @@
+<?php
+if (!defined('_GNUBOARD_')) exit;
+$bo_subject='영화';
+$bo_1='조작된 도시';
+$list=array (
+)?>
