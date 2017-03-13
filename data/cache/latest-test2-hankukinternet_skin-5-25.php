f6481d433197d80bf1a434c16cb9cb3b76a1c1ee
yoo hyun seog <dbghwns3@gmail.com>
2017-03-13 12:40:26 +0900

    test

--- data/cache/latest-test2-hankukinternet_skin-5-25.php
+++ data/cache/latest-test2-hankukinternet_skin-5-25.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('_GNUBOARD_')) exit;
 $bo_subject='영화';
-$bo_1='조작된 도시';
+$bo_1='';
 $bo_10='AIzaSyClUC-kVxJtYDxjraNMHEOebqn032h_-1M';
 $bo_9='6e05d21ca6087df839db317c3579599c';
 $list=array (
