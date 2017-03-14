ef285fe8fde0368bb00a4c7d6bb9fc42aa9d146a
yoo hyun seog <dbghwns3@gmail.com>
2017-03-15 06:18:07 +0900

    movie 0.1ver

--- theme/hankukinternet/skin/latest/hankukinternet_skin_jquery_1/latest.skin.php
+++ theme/hankukinternet/skin/latest/hankukinternet_skin_jquery_1/latest.skin.php
@@ -9,8 +9,8 @@ add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 
     <strong class="lt_title"><a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=<?php echo $bo_table ?>"><?php echo $bo_subject; ?></a></strong>
      <ul id="movie_1">
     <script type="text/javascript">
-    var count = 0;
-	var rENDER_START = 50; //rENDER 함수 // 스타트 타이머 
+    var count = 0; //전역 변수
+	var rENDER_START = 250; //rENDER 함수 // 스타트 타이머 
 	var rENDER_END = 800;  //rENDER 함수 // 종료 타이머 
 	
       function makeRequest(q) {
@@ -32,12 +32,14 @@ add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 
           $('#results').empty();
           var resultItems = response.result.items;
           $.each(resultItems, function(index, item) {
-            vidTitle = "<br><td>"+item.snippet.title+"</td></br>";
-            vidCount = "<br></td>조회수</td></br>";
+            vidTitle = "<tr><td>"+item.snippet.title+"</td></tr>";
             vidId = item.id.videoId;
+            var dateF = dateFormat(item.snippet.publishedAt);
+           	//vidYoutuI
+           	vidPublishedAt = "<tr><td>등록일: "+dateF+"</td></tr>";
             vidThumburl =  item.snippet.thumbnails['default'].url;
             vidThumbimg = '<pre><img id="thumb" src="'+vidThumburl+'" alt="No  Image Available."></pre>';
-            $('#movie_1').append('<li id="'+vidId+'">' + vidThumbimg + vidTitle + vidCount + '</li>');
+            $('#movie_1').append('<li id="'+vidId+'"><table>' + vidThumbimg +  vidTitle + vidPublishedAt +'</table></li>');
             staticsRequest(vidId)
           });
         });
@@ -54,7 +56,8 @@ add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 
 	        	var resultItems = response.result.items;
 	        	$.each(resultItems, function(index, item) {
 	        	viewCount = item.statistics.viewCount;
-	        	$('#'+vidId).append("<br><td>조회수: "+viewCount+"</td></br>");
+	        	viewCount = comma(viewCount);
+	        	$('#'+vidId).append("<tr><td>조회수: "+viewCount+"</td></tr>");
 	            });
 	        });
 	        return viewCount;
@@ -80,16 +83,16 @@ add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 
     	        	 query = data;
     	        }
     	   });
-     	   
     		var str = query;
     		var str1 = str.boxOfficeResult.boxofficeType;
     		var date = str.boxOfficeResult.showRange;
     		var str1_count = str.boxOfficeResult.boxofficeType.length;
-			
+    		$('#movie_1').append("<li id='boxoffice'><table><tr><th>이번주 박스오피스 순위</th><th>제목</th></tr></li>");
     		var rENDER = setInterval(function() {
     	        // logic
     	        if (count < str1_count) {
     	        	var dd = str.boxOfficeResult.dailyBoxOfficeList[count].movieNm;
+    	        	$("#boxoffice").append("<tr><td style='width:100px; height:30px;'>"+(count+1)+" </td><td>"+dd+"</td></tr></table>");
         			makeRequest(dd+" "+'<?=$bo_1?>');
     	        }else {
     	        	clearInterval(rENDER);
@@ -97,6 +100,44 @@ add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 
     	        count++;
     	    }, rENDER_START);
       }
+
+      function comma(num){
+  		var len, point, str;  
+  		num = num + "";  
+  		point = num.length % 3 ;
+  		len = num.length;  
+  		str = num.substring(0, point);  
+  		while (point < len) {  
+  		    if (str != "") str += ",";  
+  		    str += num.substring(point, point + 3);  
+  		    point += 3;  
+  		}
+  		return str
+  		}
+
+	  function dateFormat(str){
+		  var now = new Date(str);
+			 var year = now.getFullYear();
+	         var month = now.getMonth()+1;
+	         var day = now.getDate();
+	         var nowHour = now.getHours();
+	         var nowMt = now.getMinutes();
+	         var second = now.getSeconds();
+	         month = month < 10 ? "0" + month : month;
+	         day = day < 10 ? "0" + day : day;
+	         
+	         nowHour = nowHour < 10 ? "0" + nowHour : nowHour;
+	         nowHour = nowHour > 0 && nowHour<5 ? "새벽 "+ nowHour:nowHour; 
+	         nowHour = nowHour>4 && nowHour<9 ? "아침 "+nowHour:nowHour;
+	         nowHour = nowHour>8 && nowHour<12 ? "오전"+nowHour:nowHour;
+	         nowHour = nowHour==12 ? "정오" + nowHour:nowHour;
+	         nowHour = nowHour>12 && nowHour<18 ? "오후 "+(nowHour-12):nowHour;
+	         nowHour = nowHour>17 && nowHour<22 ? "저녁 "+(nowHour-12):nowHour;
+	         nowHour = nowHour>21 && nowHour<24 ? "밤 "+(nowHour-12):nowHour;
+	         
+	    	 var fDay = year+"년 "+month+"월 "+day+"일 "+nowHour+"시 "+nowMt+"분 "+second+"초";
+	    	 return fDay;
+		  }
       
       function init() {
         gapi.client.setApiKey('<?=$bo_10?>');
@@ -151,7 +192,6 @@ pre {
     overflow-x: visible;
     font-size: 1.1em;
 }
-
 </style>  
 </div>
  <div class="lt_more"><a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=<?php echo $bo_table ?>"><span class="sound_only"><?php echo $bo_subject ?></span>더보기</a></div>
