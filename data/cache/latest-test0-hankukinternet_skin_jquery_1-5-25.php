88377d05d72f26d48f28c66e6c253ff9a69bce84
yoo hyun seog <dbghwns3@gmail.com>
2017-03-15 02:32:51 +0900

    교차 함수 로직 - > rENDER - START
    X가 대수적으로 닫힌 체 {\displaystyle K} K에 대한 비특이 준사영 대수다양체라고 하며, {\displaystyle
    \dim _{K}X=n} \dim _{K}X=n이라고 하고, {\displaystyle x\in X} x\in X라고 하자.
    {\displaystyle Z_{1},\dots ,Z_{n}\subset X} Z_{1},\dots ,Z_{n}\subset X가
    {\displaystyle X} X의 {\displaystyle n} n개의 초곡면(여차원이 1인 부분 대수 다양체)들이며,
    이들이 {\displaystyle x} x 근처에서 국소 방정식
    
    {\displaystyle f_{i}(t_{1},\dots ,t_{n})=0\qquad (i=1,\dots ,n)}
    f_{i}(t_{1},\dots ,t_{n})=0\qquad (i=1,\dots ,n)
    으로 정의된다고 하자. 또한, 다음 조건들이 성립한다고 하자.
    
    {\displaystyle x\in \bigcap _{i=1}^{n}Z_{i}} x\in \bigcap
    _{{i=1}}^{n}Z_{i}. 즉, {\displaystyle f_{i}(x)=0\qquad (i=1,\dots ,n)}
    f_{i}(x)=0\qquad (i=1,\dots ,n)이다.
    {\displaystyle f_{i}} f_{i}는 {\displaystyle x} x에서 특이점을 갖지 않는다.
    (일반 위치 조건 영어: general position) {\displaystyle \dim _{x}\bigcap
    _{i=1}^{n}Z_{i}=0} \dim _{x}\bigcap _{{i=1}}^{n}Z_{i}=0
    이 경우, {\displaystyle x} x에서의 {\displaystyle \{Z_{i}\}_{i=1,\dots ,n}}
    \{Z_{i}\}_{{i=1,\dots ,n}}의 교차수는 다음과 같다.
    
    {\displaystyle (Z_{1},\dots ,Z_{n})_{x}=\dim _{K}{\mathcal
    {O}}_{X,x}/(f_{1},\dots ,f_{n})} (Z_{1},\dots ,Z_{n})_{x}=\dim
    _{K}{\mathcal  O}_{{X,x}}/(f_{1},\dots ,f_{n})
    여기서 {\displaystyle {\mathcal {O}}_{X,x}} \mathcal O_{X,x}는
    {\displaystyle X} X의 구조층의 {\displaystyle x} x에서의 줄기인 국소환이며,
    {\displaystyle (f_{1},\dots ,f_{n})} (f_{1},\dots ,f_{n})은 이들 다항식으로 생성되는
    {\displaystyle {\mathcal {O}}_{X,x}} \mathcal O_{X,x}의 아이디얼이다. 이 아이디얼에
    대한 몫환은 {\displaystyle K} K-벡터 공간을 이루며, {\displaystyle \dim _{K}} \dim
    _{K}는 {\displaystyle K} K-벡터 공간으로서의 차원이다.
    
    이제, 일반 위치에 있는 {\displaystyle Z_{i}} Z_{i}들의 교차수는 각 점에서의 교차수들의 합이다.
    
    {\displaystyle (Z_{1}\cdots ,Z_{n})=\sum _{x\in \bigcap
    _{i}Z_{i}}(Z_{1}\cdots Z_{n})_{x}} (Z_{1}\cdots ,Z_{n})=\sum _{{x\in
    \bigcap _{i}Z_{i}}}(Z_{1}\cdots Z_{n})_{x}
    이는 유한함을 보일 수 있다.
    
    효과적 인자들은 초곡면들의 형식적 선형 결합이므로, 일반 위치에 있는 효과적 인자의 교차수는 (일반 위치의) 초곡면들의 교차수를
    선형으로 확대하여 정의한다. 임의의 인자는 두 효과적 인자의 차로 나타낼 수 있으므로, 일반 위치에 있는 인자들의 교차수는 효과적
    인자의 교차수를 선형으로 확대하여 정의한다. 임의의 인자들의 집합은 저우 움직임 보조정리를 사용하여, 유리 동치인 일반 위치
    인자로 대체할 수 있으므로, 이에 대하여 교차수를 정의할 수 있다.
    
    study

--- data/cache/latest-test0-hankukinternet_skin_jquery_1-5-25.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!defined('_GNUBOARD_')) exit;
-$bo_subject='영화';
-$bo_1='영화 최신';
-$bo_10='AIzaSyClUC-kVxJtYDxjraNMHEOebqn032h_-1M';
-$bo_9='6e05d21ca6087df839db317c3579599c';
-$list=array (
-)?>
